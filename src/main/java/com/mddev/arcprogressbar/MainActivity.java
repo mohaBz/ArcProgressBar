eae0753cb1599434982d79f390a6d2d272fab131
bzmoha <bzmoha99@gmail.com>
2020-04-08 15:06:52 +0000

    Initial commit

---
+++ src/main/java/com/mddev/arcprogressbar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mddev.arcprogressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+private ArcProgressBar arcProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
